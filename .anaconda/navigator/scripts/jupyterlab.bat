a39f51c49c98124c45e89a3b313fd2f2d87c35a2
OnkarKulkarni5404 <onkarkulkarni5404@gmail.com>
2020-11-03 00:03:03 +0530

    Changed Bean struct

---
+++ .anaconda/navigator/scripts/jupyterlab.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\Onkar\Anaconda3\Scripts\activate C:\Users\Onkar\Anaconda3
+jupyter lab >C:\Users\Onkar\.anaconda\navigator\scripts\jupyterlab-out-1.txt 2>C:\Users\Onkar\.anaconda\navigator\scripts\jupyterlab-err-1.txt
---
+++ .anaconda/navigator/scripts/spyder.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\Onkar\Anaconda3\Scripts\activate C:\Users\Onkar\Anaconda3
+spyder >C:\Users\Onkar\.anaconda\navigator\scripts\spyder-out-2.txt 2>C:\Users\Onkar\.anaconda\navigator\scripts\spyder-err-2.txt
